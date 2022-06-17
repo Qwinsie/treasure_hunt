383568e54e503f5e14902d0c6123a9b3eca5ea73
tijs <tijs.de.graaff@hva.nl>
2022-06-17 22:51:44 +0200

    Example navigation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,11 @@
 <script>
   import Button from "../components/Button.svelte";
   import Card from "../components/Card.svelte";
+  import { goto } from "$app/navigation"
 </script>
 
 <main>
+  <button on:click={() => goto("/players")}>Navigate</button>
   <Button title="Back" />
   <Button title="Level 1" />
   <Card index="1" showButton={false} />
