92b35da9f2be1a048a1d7f2bacb6dcac4646b9b4
Qwinsie <theqpowerrr@gmail.com>
2022-06-17 21:53:37 +0200

    added Card component.

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,10 @@
 <script>
   import Button from "./components/Button.svelte";
+  import Card from "./components/Card.svelte";
 </script>
 
 <main>
-  <Button />
+  <Button title="Back" />
+  <Button title="Level 1" />
+  <Card index="1" showButton={false} />
 </main>
